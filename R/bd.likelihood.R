2706f9deef1766e4e1c265cd91ae6418e094b11e
Rachel Warnock <rachel.warnock@gmail.com>
2016-11-17 15:30:56 +0100

    Combined likelihood function added

--- R/bd.likelihood.R
+++ R/bd.likelihood.R
@@ -1,4 +1,6 @@
 
+######## fossil range estimates
+
 #' @export
 est.bd.ranges<-function(b,d,frs,lower.b=0.001,upper.b=10,lower.d=0.001,upper.d=10){
   b<-b # starting value
@@ -28,7 +30,7 @@ bd.likelihood.est.ranges<-function(p,frs){
   #eof
 }
 
-########
+######## phylogenetic estimates
 
 #' @export
 est.bd.extant<-function(b,d,tree,lower.b=0.001,upper.b=10,lower.d=0.001,upper.d=10){
@@ -55,14 +57,46 @@ bd.likelihood.est.extant<-function(p,tree){
   tree<-tree
 
   if((b < d) || (b == d))
-    return(-10000000)
+    return(-10^100)
 
   lk=bd.probability.extant(tree,b,d)
   return(lk)
   #eof
 }
 
-# constrOptim(c(1,0.1),bd.likelihood.est.extant,NULL,ui=rbind(c(-1,0),c(0,-1),c(1,0),c(0,1),c(1,-1)),
-#             ci=c(-10,-10,0.001,0.001,0.001),
-#             tree=tmp[[1]],control=list(fnscale=-1,maxit=10000)
-# )
+######## combined estimates
+#' @export
+est.bd.combined<-function(b,d,b.star,d.star,tree,frs,
+                         lower.b=0.001,upper.b=10,lower.d=0.001,upper.d=10,
+                         lower.b.star=0.001,upper.b.star=10,lower.d.star=0.001,upper.d.star=10,
+                         constrained=FALSE){
+  b<-b # starting value
+  d<-d # starting value
+  b.star<-b.star # starting value
+  d.star<-d.star # starting value
+
+  est<-optim(c(b,d,b.star,d.star),bd.likelihood.est.combined,tree=tree,frs=frs,control=list(fnscale=-1,maxit=20000), # fnscale=-1 tells the function to maximise
+             method="L-BFGS-B", # this method allows bounds on parameters
+             lower=c(lower.b,lower.d,lower.b.star,lower.d.star),
+             upper=c(upper.b,upper.d,upper.b.star,upper.d.star)
+  )
+
+  return(est)
+  # eof
+}
+
+bd.likelihood.est.combined<-function(p,tree,frs){
+  b=p[1]
+  d=p[2]
+  b.star=p[3]
+  d.star=p[4]
+  tree<-tree
+  frs<-frs
+
+  if((b < d) || (b == d))
+    return(-10^100)
+
+  lk = bd.probability.range(frs,b.star,d.star) + bd.probability.extant(tree,b,d)
+
+  return(lk)
+}
