9c13916828d80d0ac4c64f5cc1f38f6ec11e171d
Rachel Warnock <rachel.warnock@gmail.com>
2017-01-23 10:03:06 +0100

    Updating plotting functions, adding utils

--- R/plot.interval.rates.R
+++ R/plot.interval.rates.R
@@ -1,28 +1,29 @@
 
-# function to plot the output from independent methods: x-axis = absolute time, multiple options for plotting the the proportion of useful replicates
+# the following functions can be applied to output generate using the function replicates.main
+
+# function to plot the output from replicates.main: x-axis = absolute & realtive time, multiple options for plotting the the proportion of useful replicates
 #' @export
-#'
-#'
-plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.true=mu,psv=F,in3,abs=F,trees,upp=1){
+draw.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.true=mu,psv=F,sampling,abs=F,trees,upp=1){
   h.rates<-in1 # interval estimates
   t.rates<-in2 # rate estimates
   title<-title # graph title
-  col<-col #????
+  col<-col # polygon color
   p.true<-p.true # true speciation
   q.true<-q.true  # true extinction
   # for variable extinction rates
   if(length(p.true) > 1) {
     p.true=p.true[1]
   }
-  # option for plotting ????
+  # plot sampling value
   if(psv){
-    ps.vals<-in3
+    ps.vals<-sampling
   }
-  # if ab = T, use absolute not relative time -- presently inactive ????
-  if(abs){
-    trees<-trees
-  }
-  upp<-upp # options = 1,2,3
+  # if ab = T, use absolute not relative time -
+  # option not active
+  #if(abs){
+  #  trees<-trees
+  #}
+  upp<-upp # options = 1-4 for plotting % of useful replicates
   # -------
   if (col=="grey75"){
     col1="grey75"
@@ -52,7 +53,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
     col1=col
     col2=col
   }
-  # ?????
+  # color of true lambda, mu values
   col.tb=rgb(0,0,0, alpha=0.6)
 
   # -------
@@ -64,13 +65,12 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
   # replace Inf estimates values with NaN
   t.rates$p.total[t.rates$p.total==Inf]<-NaN
   valid=t.rates$p.total[which(t.rates$p.total!="NaN")]
-  # calculate the mean
-  # calculate the standard deviation #???? I think this is now the 95% HPDs
+  # calculate the mean & CIs
   #s=round(sd(t.rates$p.total,na.rm=T),2)
   #n=length(t.rates$p.total)
   if(length(valid) > 1){
     m=round(mean(t.rates$p.total,na.rm=T),2)
-    hpds=HPDinterval(as.mcmc(t.rates$p.total))
+    hpds=coda::HPDinterval(coda::as.mcmc(t.rates$p.total))
     lower=round(hpds[1],2)
     upper=round(hpds[2],2)
     tree.p=paste("Tree wide estimate = ",m," (",lower,", ",upper,")",sep="")
@@ -83,7 +83,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
   valid=t.rates$q.total[which(t.rates$q.total!="NaN")]
   if(length(valid) > 1){
     m=round(mean(t.rates$q.total,na.rm=T),2)
-    hpds=HPDinterval(as.mcmc(t.rates$q.total))
+    hpds=coda::HPDinterval(coda::as.mcmc(t.rates$q.total))
     lower=round(hpds[1],2)
     upper=round(hpds[2],2)
     tree.q=paste("Tree wide estimate = ",m," (",lower,", ",upper,")",sep="")
@@ -105,14 +105,13 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
       hp=hp+1
       # replace Inf values with NaN ## this issue requires further attention
       h.rates[[t]][h.rates[[t]]==Inf]<-NaN
-      # calculate the mean for each bin, excluding NaN/missing values
+      # calculate the mean and sd for each bin, excluding NaN/missing values
       #m=mean(h.rates[[t]],na.rm=T)
-      # the standard deviation for each bin, excluding NaN/missing values
-      #s=sd(h.rates[[t]],na.rm=T) # ????
+      #s=sd(h.rates[[t]],na.rm=T)
       hr=na.omit(h.rates[t])
       if(length(hr[,1]) > 1) {
         m=mean(hr[,1])
-        hpds=HPDinterval(as.mcmc(hr))
+        hpds=coda::HPDinterval(coda::as.mcmc(hr))
         lower=hpds[1]
         upper=hpds[2]
         useful=length(hr[,1])/length(h.rates[[t]])
@@ -136,7 +135,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
       hr=na.omit(h.rates[t])
       if(length(hr[,1]) > 1) {
         m=mean(hr[,1])
-        hpds=HPDinterval(as.mcmc(hr))
+        hpds=coda::HPDinterval(coda::as.mcmc(hr))
         lower=hpds[1]
         upper=hpds[2]
         useful=length(hr[,1])/length(h.rates[[t]])
@@ -189,13 +188,13 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
       min.l=0
     }
   }
-  if(upp==4){ #????
+  if(upp==4){
     par(fig=c(0,0.33,0.625,1))
   }
   plot(df$h, df$mean, ylim = c(min.l,max.u), xlim=c(0,max(df$h)), type="n",xlab="",ylab="Speciation rate HPD",bty="n",xaxt="n",tcl=-0.2)
   lines(df$h,y=rep(p.true,length(df$h)),col=col.tb,lwd=3,lty=3) # true speciation rate
-  plot.polygons(polygons.p,col=col) # add color option
-  title(title,line=0.5,cex.main=1.2,adj=0,font.main=1) # **title options
+  draw.polygons(polygons.p,col=col) # add color option
+  title(title,line=0.5,cex.main=1.2,adj=0,font.main=1) # title options
   # useful proportion
   if(upp==1){
     lines(df$h,y=df$use,col="darkgrey",lwd=3,lty=1)
@@ -213,8 +212,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
   }
   else if (upp==4){
     par(fig=c(0,0.33,0.5,0.625),new=T)
-    #plot(df$h, df$mean, ylim = c(0,1), xlim=c(0,max(df$h)), type="n",xlab="",ylab="% URs",bty="n",xaxt="n",tcl=-0.2) #???? delete
-    plot(df$h, df$mean, ylim = c(0,1), xlim=c(0,max(df$h)), type="n",xlab=NA,ylab=NA,bty="n",xaxt="n",yaxt="n")
+    plot(df$h, df$mean, ylim = c(0,1), xlim=c(0,max(df$h)), type="n",xlab=NA,ylab=NA,bty="n",xaxt="n",yaxt="n") # tcl=-0.2
     lines(df$h,y=df$use,col="darkgrey",lwd=3,lty=1)
     axis(side=2,at=c(0,0.5,1),tcl=-0.2)
     mtext(side=2,"% URs",cex=.65,line=1.5)
@@ -245,8 +243,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
   }
   for (t in 1:length(h.rates$t20.q)) {
     y=h.rates[t,][seq(from=2,to=40,by=2)]
-    #lines(x=df$h,y=y,col=col1,type="o")
-    lines(x=df$h,y=y,col=col1)
+    lines(x=df$h,y=y,col=col1) # type="o"
   }
   lines(df$h,y=rep(p.true,length(df$h)),col=col.tb,lwd=3,lty=3) # true speciation rate
   text(10,max.u+.5,tree.p)
@@ -267,14 +264,8 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
   # note the y-axis limits do not need to change
   # define the maximum limit on the x-axis
   basin.age.max=round(max(trees[[2]]),1)+0.1 # define the maximum basin age
-  #bam=max(unlist(lapply(1:100,function(x){ max(n.ages(trees[[x]])) } )))
-  #basin.age.max=round(bam,1)+0.1
-  #if(basin.age.max < 10){
-  ba.mx=round_any(basin.age.max,5,ceiling)
-  #}
-  #else{
-  #  ba.mx=round_any(basin.age.max,20,ceiling)
-  #}
+  ba.mx=plyr::round_any(basin.age.max,5,ceiling)
+
   # define an arbitrary set of horizon ages
   horizon.ages.arb=seq(from=basin.age.max,to=0,length.out=20)
   # initiate the plot -- note df$mean here is not meaningful
@@ -324,7 +315,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
     lines(x=c(0,1,2),y=rep(q.true[1],3),col=col.tb,lwd=3,lty=3)
     lines(x=c(18,19,20),y=rep(q.true[1],3),col=col.tb,lwd=3,lty=3)
   }
-  plot.polygons(polygons.q,col=col) # add color option
+  draw.polygons(polygons.q,col=col) # add color option
   # useful proportion
   if(upp==1){
     lines(df$h,y=df$use,col="darkgrey",lwd=3,lty=1)
@@ -387,17 +378,7 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
   if(upp==4){
     par(fig=c(0.66,1,0,0.5),new=T)
   }
-  # define the maximum limit on the x-axis
-  # what's going on here? ???? delete? how *does the fxn" calculate the max?
-  #basin.age.max=round(max(trees[[2]]),1)+0.1 # define the maximum basin age
-  #if(basin.age.max < 10){
-  #  ba.mx=round_any(basin.age.max,10,ceiling)
-  #}
-  #else{
-  #  ba.mx=round_any(basin.age.max,20,ceiling)
-  #}
-  # define an arbitrary set of horizon ages
-  #horizon.ages.arb=seq(from=ba.mx,to=0,length.out=20)
+  # the maximum limit on the x-axis is define above
   # initiate the plot -- note df$mean here is not meaningful
   plot(horizon.ages.arb, df$mean, ylim = c(min.l,max.u+1), xlim=c(ba.mx,0), type="n",xlab="Absolute tree age",ylab="",bty="n",tcl=-0.2)
   # plot the estimates for each replicate
@@ -416,22 +397,133 @@ plot.p.q.est.ab<-function(in1,in2,title="Title",col="grey75",p.true=lambda,q.tru
     lines(x=c(5.05,basin.age.max),y=rep(q.true[1],2),col=col.tb,lwd=3,lty=3) # I think x should really be based on horizon.ages.arb -- e.g. basin.age.max, basin.age.max-1, basin.age.max-2
   }
   else if(length(q.true)==3){
-    #me.rate=q.true[2] # ???? delete?
     lines(x=c(0,5,5.1,5.05,basin.age.max),y=c(q.true[1],q.true[1],q.true[2],q.true[1],q.true[1]),col=col.tb,lwd=3,lty=3) # x final should really be based on
   }
   # -------
   #eof
 }
 
+# function to generate subsets of polygons
+#' @export
+polygon.subsets<-function(input){
+  df<-input
+
+  k=1 # polygon counter
+  polygons=list()
+  g<-data.frame(h=numeric(),mean=numeric(),L=numeric(),U=numeric())
+
+  for(i in 1:length(df$mean)) {
+
+    m=df$mean[i]
+
+    # if mean != NaN collect the polygon info
+    if(is.nan(m)==0){
+      # if mean < Inf: add values to polygon k
+      if ((m > 0) & (m < Inf)){
+        g=rbind(g,data.frame(h=df$h[i],mean=df$mean[i],L=df$L[i],U=df$U[i]))
+      }
+      # else create a new polygon
+      else {
+        polygons[[k]]=g
+        k=k+1
+        g<-data.frame(h=numeric(),mean=numeric(),L=numeric(),U=numeric())
+      }
+    }
+    else {
+      polygons[[k]]=g
+      k=k+1
+      g<-data.frame(h=numeric(),mean=numeric(),L=numeric(),U=numeric())
+    }
+
+    if(i==length(df$mean)) {
+      polygons[[k]]=g
+    }
+  }
+  return(polygons)
+  #eof
+}
+
+# function to plot polygons
+#' @export
+draw.polygons<-function(polygons,col="grey75"){
+  polygons<-polygons
+  col<-col
+
+  if (col=="grey75"){
+    col1="grey75"
+    col2="red"
+  }
+  else if(col=="red"){
+    col1=rgb(255/255,51/255,51/255, alpha=0.5)
+    col2="red"
+  }
+  else if(col=="blue"){
+    col1=rgb(0/255,128/255,255/255, alpha=0.5)
+    col2="blue"
+  }
+  else if(col=="green"){
+    col1=rgb(76/255,153/255,0, alpha=0.5)
+    col2="green"
+  }
+  else if(col=="orange"){
+    col1=rgb(255/255,200/255,0/255, alpha=0.5)
+    col2=rgb(255/255,205/255,0/255)
+  }
+  else if(col=="purple"){
+    col1=rgb(102/255,0/255,51/255, alpha=0.5)
+    col2=rgb(102/255,0/255,51/255)
+  }
+  else {
+    col1=col
+    col2=col
+  }
+
+  for (i in 1:length(polygons)) {
+
+    sub.polygon=polygons[[i]]
+
+    # skip polygon if n < 1 e.g. polygon is empty
+
+    if(length(sub.polygon$h) == 1) {
+      age1=sub.polygon$h-0.1
+      age2=sub.polygon$h+0.1
+      lower=sub.polygon$L
+      upper=sub.polygon$U
+      mean=sub.polygon$mean
+      rect(age1,lower,age2,upper,col=col1,border=F)
+      lines(c(age1,age2),c(mean,mean),lwd = 2)
+      lines(c(age1,age2),c(lower,lower), col=col2)
+      lines(c(age1,age2),c(upper,upper), col=col2)
+    }
+
+    if(length(sub.polygon$h) > 1) {
+      polygon(c(sub.polygon$h,rev(sub.polygon$h)),c(sub.polygon$L,rev(sub.polygon$U)),col=col1, border = FALSE)
+      lines(sub.polygon$h, sub.polygon$mean, lwd = 2)
+      lines(sub.polygon$h, sub.polygon$U, col=col2,lty=2)
+      lines(sub.polygon$h, sub.polygon$L, col=col2,lty=2)
+    }
+
+  }
+  #eof
+}
+
+# function to plot sampling probability
+#' @export
+tree.reps.ps<-function(in1){
+  ps.values<-in1
+
+  # replace Inf estimates values with NaN
+  ps.values$ps[ps.values$ps==Inf]<-NaN
+  # calculate the mean
+  m = round(mean(ps.values$ps,na.rm=T),2)
+  # calculate the standard deviation
+  s = round(sd(ps.values$ps,na.rm=T),2)
+  tree.ps=paste("Pr(s) =",m,"+/-",s)
+
+  return(tree.ps)
+  #eof
+}
+
+is.wholenumber<-function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
 
 
-
-# plot.p.q.est.ab
-#
-# #plot.p.q.est = alternative
-#
-# use :: for coda functions
-# polygon.subsets
-# experiment with the upp and write documentation
-# plot.polygons
-# tree.reps.ps
